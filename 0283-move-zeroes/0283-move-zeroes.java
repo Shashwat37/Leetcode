387202f4a4a031c160a8a43932b74b2fad78a645
Shashwat Verma <shashwatv37@gmail.com>
2026-06-29 23:11:06 +0530

    Time: 2 ms (92.01%), Space: 48 MB (19.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int temp;
+        int j=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                    temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    j++;
+            }
+        }
+    }
+}
