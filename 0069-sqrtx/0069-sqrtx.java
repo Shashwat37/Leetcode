14e03d84852945a2ab697abd2fddfcf9fc0edbe4
Shashwat Verma <shashwatv37@gmail.com>
2026-07-30 19:35:50 +0530

    Time: 1 ms (97.43%), Space: 42.4 MB (79.26%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int l = 1;
+        int r = x / 2;
+        int ans = 0;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if ((long) m * m <= x) {
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return ans;
+    }
+}
