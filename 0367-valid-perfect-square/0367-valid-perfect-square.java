2a72bc0b400ee212864d86ba9226fa991a08d0e4
Shashwat Verma <shashwatv37@gmail.com>
2026-08-28 14:51:41 +0530

    Time: 0 ms (100%), Space: 42.2 MB (39%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l = 1;
+        long r = num;
+
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+
+            if (m * m == num) {
+                return true;
+            }
+
+            if (m * m < num) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return false;
+    }
+}
