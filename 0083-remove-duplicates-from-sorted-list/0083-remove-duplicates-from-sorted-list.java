ba1083c99c4667f8d8a13e44f714fa79e9e4c80b
Shashwat Verma <shashwatv37@gmail.com>
2026-09-16 22:44:48 +0530

    Time: 0 ms (100%), Space: 45.1 MB (92.93%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -8,61 +8,19 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
 
-        HashMap<ListNode, Integer> map = new HashMap<>();
-
         ListNode temp = head;
-        int index = 0;
 
-        while (temp != null) {
-            if (!map.containsKey(temp)) {
-                map.put(temp, index);
-                index++;
-            }
-            temp = temp.next;
-        }
-
-        ListNode[] arr = new ListNode[map.size()];
-
-        int i = 0;
-        for (ListNode node : map.keySet()) {
-            arr[i] = node;
-            i++;
-        }
-
-        for (i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j].val > arr[j + 1].val) {
-                    ListNode t = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = t;
-                }
-            }
-        }
-
-        ListNode newHead = null;
-        ListNode tail = null;
-
-        for (i = 0; i < arr.length; i++) {
-
-            if (i > 0 && arr[i].val == arr[i - 1].val) {
-                continue;
-            }
-
-            arr[i].next = null;
-
-            if (newHead == null) {
-                newHead = arr[i];
-                tail = arr[i];
+        while (temp != null && temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;
             } else {
-                tail.next = arr[i];
-                tail = arr[i];
+                temp = temp.next;
             }
         }
 
-        return newHead;
+        return head;
     }
 }
